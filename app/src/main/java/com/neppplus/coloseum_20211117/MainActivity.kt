87c92ea2b1fb13cc96cb658a5182ed8b769b0f77
ujin-Bang <qkddnwls14@gmail.com>
2021-11-17 16:41:34 +0900

    서버 로그인 API호출  - 응답 내용  String -> JSONObject 변환  - 로그인 확인(한글 다시 변환 ok)

--- app/src/main/java/com/neppplus/coloseum_20211117/MainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211117/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.coloseum_20211117.databinding.ActivityMainBinding
+import com.neppplus.coloseum_20211117.utils.ServerUtil
 
 class MainActivity : BaseActivity() {
 
@@ -16,14 +17,21 @@ class MainActivity : BaseActivity() {
         setValues()
         setupEvents()
 
+
+
     }
 
     override fun setupEvents() {
 
         binding.btnLogin.setOnClickListener {
 
+
             val inputEmail = binding.edtEmail.text.toString()
             val inputPw = binding.edtPassword.text.toString()
+
+
+//            서버에서 이메일 비번이 맞는 계정인지? 로그인 요청
+            ServerUtil.postRequestLogIn(inputEmail, inputPw)
         }
     }
 
