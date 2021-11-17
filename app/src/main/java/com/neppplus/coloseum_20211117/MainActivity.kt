d1ea2477feee111359ce77f6a2c4b58cd9dbd552
ujin-Bang <qkddnwls14@gmail.com>
2021-11-17 17:35:14 +0900

    오늘의 마지막 에러 강사님이 에러 잡아줌

--- app/src/main/java/com/neppplus/coloseum_20211117/MainActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211117/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.coloseum_20211117.databinding.ActivityMainBinding
 import com.neppplus.coloseum_20211117.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -33,29 +34,33 @@ class MainActivity : BaseActivity() {
 
 
 //            서버에서 이메일 비번이 맞는 계정인지? 로그인 요청
-            ServerUtil.postRequestLogIn(inputEmail, inputPw, object : ServerUtil.jsonResponseHandler){
-                onResponse(jsonObj: JSONObject){
+            ServerUtil.postRequestLogIn(inputEmail, inputPw, object : ServerUtil.jsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
 
 
                     Log.d("화면에서의 jsonObj",jsonObj.toString())
-                    
+
                     val code = jsonObj.getInt("code")
-                    
-                   runOnUiThread {
 
-                       if(code == 200) {
-                           Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
-                       }
-                       else {
-                           Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
-                       }
+                    runOnUiThread {
 
-                   }
+                        if(code == 200) {
+                            Toast.makeText(mContext, "로그인 성공", Toast.LENGTH_SHORT).show()
+                        }
+                        else {
+                            Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+
+//                           message String으로 실패 사유를 알려준다
+//                           JSON 파싱으로 추출해서 -> 로그인 실패 대신 서버가 알려준 실패 사유를 띄우자
+                            Toast.makeText(mContext, "로그인실패 ", Toast.LENGTH_SHORT).show()
+                        }
+
+                    }
 
 
                 }
 
-            })
+            } )
         }
     }
 
