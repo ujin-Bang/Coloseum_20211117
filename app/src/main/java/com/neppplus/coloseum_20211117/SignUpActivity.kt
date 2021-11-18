d276b849fe9aa29732adf226d162e1557e8650a0
ujin-Bang <qkddnwls14@gmail.com>
2021-11-18 10:08:54 +0900

    회원가입 API 호출 기능 추가

---
+++ app/src/main/java/com/neppplus/coloseum_20211117/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package com.neppplus.coloseum_20211117
+
+class SignUpActivity {
+
+
+}
+
+
+ fun setupEvents( ) {
+     binding.btnOk.setOnClickListener {
+
+         val inputEmail = binding.edtEmail.text.toString()
+
+         val inputPw = binding.edtPassword.text.toString()
+         val inputNickname = binding.edtNickname.tex
+     }
+ }
