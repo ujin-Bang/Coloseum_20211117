799db4271b4061132ce230484a008200fccb29f6
ujin-Bang <qkddnwls14@gmail.com>
2021-11-18 10:17:39 +0900

    서버 응답 분석  -> code : 200이 아니면 -> 왜 실패인지 message에 담긴 String 토스트 출력

--- app/src/main/java/com/neppplus/coloseum_20211117/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/coloseum_20211117/SignUpActivity.kt
@@ -1,5 +1,8 @@
 package com.neppplus.coloseum_20211117
 
+import android.widget.Toast
+import com.neppplus.coloseum_20211117.utils.ServerUtil
+
 class SignUpActivity {
 
 
@@ -12,6 +15,33 @@ class SignUpActivity {
          val inputEmail = binding.edtEmail.text.toString()
 
          val inputPw = binding.edtPassword.text.toString()
-         val inputNickname = binding.edtNickname.tex
+         val inputNickname = binding.edtNickname.text.toString()
+
+
+//
+         ServerUtil.putRequestSignUp(inputEmail,inputPw,inputNickname, object :ServerUtil.JsonResponseHandler) {
+            override fun onResponse(jsonObj: JSONObject) {
+
+                val code = jsonObj.getInt("code")
+
+                if( code == 200) {
+             }
+                else {
+
+//                    실패 -> 서버가 알려주는 "message"에 담긴 실패 사유를 토스트로
+                    val message = jsonObj.getString("message")
+
+                    runOnUiThread {
+
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+
+                }
+
+         }
+
      }
  }
